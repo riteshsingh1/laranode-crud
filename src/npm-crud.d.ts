28a445e6bee8850ad38a1eeadf93fcaf3c5207c3
Ritesh Singh <1124ritesh@gmail.com>
2023-09-26 18:07:49 +0530

    ad
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- src/npm-crud.d.ts
+++ src/npm-crud.d.ts
@@ -1,13 +1,5 @@
-declare module "npm0crud" {
+declare module "npm-crud" {
   import * as React from "react";
-  export interface model {
-    basename?: string;
-    getUserConfirmation?: (
-      message: string,
-      callback: (ok: boolean) => void
-    ) => void;
-    forceRefresh?: boolean;
-    keyLength?: number;
-  }
+  export interface model {}
   export class model {}
 }
